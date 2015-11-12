452d2d5e1d7b3d584672cd22bf3dda74595bdc4d
droom <chrisdroom@gmail.com>
2015-11-12 01:28:16 +0000

    adds smooth scroll, amends btns, amends anchors

--- src/js/main.js
+++ src/js/main.js
@@ -35140,11 +35140,9 @@ $.fn.visibility.settings = {
     });
 
 
-
     $('.label.bookmark i.red.heart.label.icon').on('click', function() {
       console.log("hello")
     });
-
 
     $('#toggle-bookmark').click(function(e){
       e.preventDefault();
@@ -35157,15 +35155,31 @@ $.fn.visibility.settings = {
     });
 
 
-
     $(window).scroll(function () {
-      console.log($(window).scrollTop())
+
       if ($(window).scrollTop() > 99) {
         $('#nav_bar').addClass('navbar-fixed');
       }
       if ($(window).scrollTop() < 100) {
         $('#nav_bar').removeClass('navbar-fixed');
       }
+    });
+
+
+    // smooth scrolling
+    $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 300);
+            return false;
+          }
+        }
+      });
     });
 
 
