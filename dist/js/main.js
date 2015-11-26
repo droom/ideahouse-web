1c613fd982c7844da87fc5bde36cfb124229dfb3
droom <chrisdroom@gmail.com>
2015-11-26 23:32:57 +0000

    make tiles 4 wide

--- dist/js/main.js
+++ dist/js/main.js
@@ -35640,7 +35640,32 @@ $.fn.visibility.settings = {
 	});
 
 }));
-;$(document).ready(function() {
+;/* Elementary. A CSS workflow for mimicking element queries. [c]2014 @scottjehl, @filamentgroup. MIT license. */
+(function( w ){
+
+	w.elementary = function( options ){
+		if( !( "querySelector" in w.document ) || !( "getComputedStyle" in w ) ){
+			return;
+		}
+		var selector = options && options.selector || ".mod";
+		var mods = w.document.querySelectorAll( selector );
+		for( var m = 0; m < mods.length; m++ ){
+			var mod = mods[ m ],
+				breakpoints = w.getComputedStyle( mod, ":before" ).getPropertyValue( "content" ),
+				widths = breakpoints.replace(/[^\d ]/g,"").split( " "),
+				modWidth = mod.clientWidth,
+				minWidths = [];
+
+			for( var i = 0; i < widths.length; i++ ){
+				if( w.parseFloat( widths[ i ] ) <= modWidth ){
+					minWidths.push( widths[ i ] );
+				}
+			}
+			mod.setAttribute( "data-minwidth", minWidths.join( " " ) );
+		}
+	};
+
+})( this );;$(document).ready(function() {
 
   if ($('body.bookmarks').length > 0){
     checkBookmarkTiles();
@@ -36029,6 +36054,12 @@ $.fn.visibility.settings = {
 
 
   });
+;$(document).ready(function() {
+
+  window.addEventListener( "load", window.elementary, false );
+  window.addEventListener( "resize", window.elementary, false );
+
+});
 ;  $(document).ready(function() {
 
     var searchSwatch = $('table.ui.table.swatch.search')
