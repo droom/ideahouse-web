74cedc128b6f1431d1a2e8dcb03bea7bcc95d04a
droom <chrisdroom@gmail.com>
2016-05-13 22:03:50 +0100

    changes table sorting library

---
+++ src/js/tables.js
@@ -0,0 +1,26 @@
+  $(document).ready(function() {
+
+    // Tablesorter 
+    // Default sorting
+
+    $("table").tablesorter({ 
+      sortList: [[1,1]] 
+    }); 
+
+
+    // $("table.product thead th.descending").click(function(e) {
+    //   e.preventDefault();
+    //   $(this).removeClass('descending');
+    //   $(this).addClass('ascending');
+    // });
+
+    // $("table.product thead th.ascending").click(function(e) {
+    //   e.preventDefault();
+    //   $(this).removeClass('ascending');
+    //   $(this).addClass('descending');
+    // });
+
+
+
+
+  });
